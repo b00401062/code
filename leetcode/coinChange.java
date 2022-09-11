d00e4f8a2c626ac7404213935f4259450cff0123
Wen-Bin Luo <b00401062@ntu.edu.tw>
2022-09-11 12:34:03 -0400

    Add files via upload

--- leetcode/coinChange.java
+++ leetcode/coinChange.java
@@ -1,15 +1,18 @@
-package leetcode
+package leetcode;
 
-import kotlin.math.min
+import java.util.Arrays;
 
-fun coinChange(coins: IntArray, amount: Int): Int {
-    val cache = IntArray(amount + 1) { Int.MAX_VALUE }
-    cache[0] = 0
-    for (coin in coins) {
-        for (i in coin..amount) {
-            if (cache[i - coin] == Int.MAX_VALUE) continue
-            cache[i] = min(cache[i - coin] + 1, cache[i])
+class LeetCode {
+    public static int coinChange(int[] coins, int amount) {
+        var cache = new int[amount + 1];
+        Arrays.fill(cache, Integer.MAX_VALUE);
+        cache[0] = 0;
+        for (var coin : coins) {
+            for (var i = coin; i <= amount; i++) {
+                if (cache[i - coin] == Integer.MAX_VALUE) continue;
+                cache[i] = Math.min(cache[i - coin] + 1, cache[i]);
+            }
         }
+        return cache[amount] == Integer.MAX_VALUE ? -1 : cache[amount];
     }
-    return if (cache[amount] == Int.MAX_VALUE) -1 else cache[amount]
 }
