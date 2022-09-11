c5c7b7ae95bf40dffdc03afa0610d4bb614747e0
Wen-Bin Luo <b00401062@ntu.edu.tw>
2022-09-10 20:20:59 -0400

    Add files via upload

--- leetcode/addTwoNumbers.java
+++ leetcode/addTwoNumbers.java
@@ -1,24 +1,26 @@
-package leetcode
+package leetcode;
 
-private fun carry(l: ListNode) {
-    if (l.`val` < 10)
-        return
-    l.`val` %= 10
-    if (l.next == null)
-        l.next = ListNode(1)
-    else {
-        l.next!!.`val`++
-        carry(l.next!!)
+class LeetCode {
+    private static void carry(ListNode l) {
+        if (l.val < 10)
+            return;
+        l.val %= 10;
+        if (l.next == null) {
+            l.next = new ListNode(1);
+        } else {
+            l.next.val++;
+            carry(l.next);
+        }
     }
-}
-
-fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
-    if (l1 == null)
-        return l2
-    if (l2 == null)
-        return l1
-    val l = ListNode(l1.`val` + l2.`val`)
-    l.next = addTwoNumbers(l1.next, l2.next)
-    carry(l)
-    return l
+    
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+        var l = new ListNode(l1.val + l2.val);
+        l.next = addTwoNumbers(l1.next, l2.next);
+        carry(l);
+        return l;
+    }
 }
