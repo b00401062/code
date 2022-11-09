de3e60e42a40cf2d77519d85ba8933d2a0ca1842
Wen-Bin Luo <b00401062@ntu.edu.tw>
2022-11-09 23:36:56 +0000

    Convert kt to java

--- leetcode/CombinationSum4.java
+++ leetcode/CombinationSum4.java
@@ -1,13 +1,15 @@
-package leetcode
+package leetcode;
 
-fun combinationSum4(nums: IntArray, target: Int): Int {
-    val cache = IntArray(target + 1)
-    cache[0] = 1
-    for (i in 1..target) {
-        for (num in nums) {
-            if (num > i) continue
-            cache[i] += cache[i - num]
+class CombinationSum4 {
+    public static int combinationSum4(int[] nums, int target) {
+        var cache = new int[target + 1];
+        cache[0] = 1;
+        for (var i = 1; i <= target; i++) {
+            for (var num : nums) {
+                if (num > i) continue;
+                cache[i] += cache[i - num];
+            }
         }
+        return cache[target];
     }
-    return cache[target]
 }
--- leetcode/UniquePathsWithObstacles.java
+++ leetcode/UniquePathsWithObstacles.java
@@ -1,18 +1,16 @@
-package leetcode
+package leetcode;
 
-fun uniquePathsWithObstacles(obstacleGrid: Array<IntArray>): Int {
-    val nrow = obstacleGrid.size
-    val ncol = obstacleGrid[0].size
-    val dp = Array<IntArray>(nrow + 1) { IntArray(ncol + 1) }
-    dp[0][1] = 1
-    for (i in 0 until nrow) {
-        for (j in 0 until ncol) {
-            dp[i + 1][j + 1] = when (obstacleGrid[i][j]) {
-                0 -> dp[i][j + 1] + dp[i + 1][j]
-                1 -> 0
-                else -> throw IllegalArgumentException()
+class UniquePathsWithObstacles {
+    public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        var nrow = obstacleGrid.length;
+        var ncol = obstacleGrid[0].length;
+        var dp = new int[nrow + 1][ncol + 1];
+        dp[0][1] = 1;
+        for (var i = 0; i < nrow; i++) {
+            for (var j = 0; j < ncol; j++) {
+                dp[i + 1][j + 1] = obstacleGrid[i][j] == 0 ? dp[i][j + 1] + dp[i + 1][j] : 0;
             }
         }
+        return dp[nrow][ncol];
     }
-    return dp.last().last()
 }
