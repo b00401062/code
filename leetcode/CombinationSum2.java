e0268fa821ad37e3e469cef9ee765dd8221dc36b
Wen-Bin Luo <b00401062@ntu.edu.tw>
2022-11-09 13:59:33 +0000

    Convert kt to java

--- leetcode/CombinationSum2.java
+++ leetcode/CombinationSum2.java
@@ -1,32 +1,48 @@
-package leetcode
+package leetcode;
 
-import kotlin.math.min
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.List;
 
-private var table = mapOf<Int, Int>()
+class CombinationSum2 {
+    private static Map<Integer, Integer> table;
 
-private fun recur(candidates: IntArray, target: Int): List<List<Int>> {
-    if (target == 0)
-        return listOf(emptyList())
-    else if (candidates.isEmpty())
-        return emptyList()
-    val res = mutableListOf<List<Int>>()
-    for ((i, candidate) in candidates.distinct().withIndex()) {
-        if (target < candidate)
-            break
-        else if (target == candidate) {
-            res.add(listOf(candidate))
-            break
-        } else for (n in 1..min(target / candidate, table[candidate]!!)) {
-            recur(
-                candidates.sliceArray(i + 1 until candidates.size),
-                target - candidate * n
-            ).forEach { res.add(List(n) { candidate } + it) }
+    private static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        if (target == 0)
+            return List.of(List.of());
+        else if (candidates.length == 0)
+            return List.of();
+        var res = new ArrayList<List<Integer>>();
+        for (var i = 0; i < candidates.length; i++) {
+            var candidate = candidates[i];
+            if (target < candidate)
+                break;
+            else if (target == candidate) {
+                res.add(List.of(candidate));
+                break;
+            } else for (var n = 1; n <= Math.min(target / candidate, table.get(candidate)); n++) {
+                for (var postfix : combinationSum(
+                    Arrays.copyOfRange(candidates, i + 1, candidates.length),
+                    target - candidate * n
+                )) {
+                    var prefix = Stream.generate(() -> candidate).limit(n).collect(Collectors.toList());
+                    prefix.addAll(postfix);
+                    res.add(prefix);
+                }
+            }
         }
+        return res;
     }
-    return res
-}
 
-fun combinationSum2(candidates: IntArray, target: Int): List<List<Int>> {
-    table = candidates.toList().groupingBy { it }.eachCount()
-    return recur(table.keys.sorted().toIntArray(), target)
+    public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        table = new HashMap<>();
+        for (var candidate : candidates) {
+            table.put(candidate, table.getOrDefault(candidate, 0) + 1);
+        }
+        return combinationSum(table.keySet().stream().sorted().mapToInt(x -> x).toArray(), target);
+    }
 }
