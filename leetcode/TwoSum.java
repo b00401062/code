d068182f3dfade6d184d506741106aa06da852fc
Wen-Bin Luo <b00401062@ntu.edu.tw>
2022-11-09 10:00:51 +0000

    Convert kt to java

--- leetcode/TwoSum.java
+++ leetcode/TwoSum.java
@@ -1,11 +1,16 @@
-package leetcode
+package leetcode;
 
-fun twoSum(nums: IntArray, target: Int): IntArray? {
-    val map = mutableMapOf<Int, Int>()
-    for (i in nums.indices)
-        if (map.containsKey(target - nums[i]))
-            return intArrayOf(map[target - nums[i]]!!, i)
-        else
-            map[nums[i]] = i
-    return null
+import java.util.Arrays;
+import java.util.HashMap;
+
+class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        var map = new HashMap<Integer, Integer>();
+        for (var i = 0; i < nums.length; i++)
+            if (map.containsKey(target - nums[i]))
+                return new int[] { map.get(target - nums[i]), i };
+            else
+                map.put(nums[i], i);
+        return null;
+    }
 }
