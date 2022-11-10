bd06bc10fb6d3b9752bcf5ed38261e2b166d9c8b
Wen-Bin Luo <b00401062@ntu.edu.tw>
2022-11-10 03:43:35 +0000

    Convert kt to java

--- leetcode/GenerateMatrix.java
+++ leetcode/GenerateMatrix.java
@@ -1,25 +1,27 @@
-package leetcode
+package leetcode;
 
-fun generateMatrix(n: Int): Array<IntArray> {
-    val a = Array(n) { IntArray(n) }
-    var r = 0
-    var c = 0
-    val dr: IntArray = intArrayOf(0, 1, 0, -1)
-    val dc: IntArray = intArrayOf(1, 0, -1, 0)
-    var d = 0
-    var k = 1
-    for (i in 0 until n * n) {
-        a[r][c] = k++
-        val nr = r + dr[d]
-        val nc = c + dc[d]
-        if (nc >= 0 && nr >= 0 && nc < n && nr < n && a[nr][nc] == 0) {
-            r = nr
-            c = nc
-        } else {
-            d = (d + 1) % 4
-            r += dr[d]
-            c += dc[d]
+class GenerateMatrix {
+    public static int[][] generateMatrix(int n) {
+        var a = new int[n][n];
+        var r = 0;
+        var c = 0;
+        var dr = new int[] { 0, 1, 0, -1 };
+        var dc = new int[] { 1, 0, -1, 0 };
+        var d = 0;
+        var k = 1;
+        for (var i = 0; i < n * n; i++) {
+            a[r][c] = k++;
+            var nr = r + dr[d];
+            var nc = c + dc[d];
+            if (nc >= 0 && nr >= 0 && nc < n && nr < n && a[nr][nc] == 0) {
+                r = nr;
+                c = nc;
+            } else {
+                d = (d + 1) % 4;
+                r += dr[d];
+                c += dc[d];
+            }
         }
+        return a;
     }
-    return a
 }
--- leetcode/GenerateParenthesis.java
+++ leetcode/GenerateParenthesis.java
@@ -1,31 +1,36 @@
-package leetcode
+package leetcode;
 
-import java.util.*
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.stream.Collectors;
 
-fun generateParenthesis(n: Int): List<String> {
-    val stack = Stack<Char>()
-    val list = mutableListOf<String>()
-    stack.push('(')
-    var nl = 1
-    var nr = 0
-    while (nl != 0) {
-        while (nl < n) {
-            stack.push('(')
-            nl++
+class GenerateParenthesis {
+    public List<String> generateParenthesis(int n) {
+        var stack = new Stack<Character>() {{ push('('); }};
+        var list = new ArrayList<String>();
+        var nl = 1;
+        var nr = 0;
+        while (nl != 0) {
+            while (nl < n) {
+                stack.push('(');
+                nl++;
+            }
+            while (nr < n) {
+                stack.push(')');
+                nr++;
+            }
+            list.add(stack.stream().map(String::valueOf).collect(Collectors.joining("")));
+            while (nl == nr) {
+                while (stack.pop() == ')') {
+                    nr--;
+                }
+                nl--;
+                if (nl == 0) break;
+            }
+            stack.push(')');
+            nr++;
         }
-        while (nr < n) {
-            stack.push(')')
-            nr++
-        }
-        list.add(stack.joinToString(""))
-        while (nl == nr) {
-            while (stack.pop() == ')')
-                nr--
-            nl--
-            if (nl == 0) break
-        }
-        stack.push(')')
-        nr++
+        return list;
     }
-    return list
 }
