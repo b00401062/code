8ffacc125e75469700e2fb2e09c012f04e9c5054
Wen-Bin Luo <b00401062@ntu.edu.tw>
2022-11-11 11:28:05 +0000

    Convert kt to java

--- leetcode/MoveZeroes.java
+++ leetcode/MoveZeroes.java
@@ -1,13 +1,15 @@
 package leetcode;
 
-fun moveZeroes(nums: IntArray): Unit {
-    var non_zero_count = 0
-    for (num in nums) {
-        if (num == 0) continue
-        nums[non_zero_count] = num
-        non_zero_count += 1
-    }
-    for (var idx = non_zero_count; idx < nums.size; idx++) {
-        nums[idx] = 0
+class MoveZeroes {
+    public static void moveZeroes(int[] nums) {
+        var non_zero_count = 0;
+        for (var num : nums) {
+            if (num == 0) continue;
+            nums[non_zero_count] = num;
+            non_zero_count++;
+        }
+        for (var idx = non_zero_count; idx < nums.length; idx++) {
+            nums[idx] = 0;
+        }
     }
 }
