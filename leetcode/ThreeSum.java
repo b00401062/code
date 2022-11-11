7e8ef274a8d23c1087df890e6a9f772a347f81c6
Wen-Bin Luo <b00401062@ntu.edu.tw>
2022-11-11 13:28:58 +0000

    Convert kt to java

--- leetcode/ThreeSum.java
+++ leetcode/ThreeSum.java
@@ -1,25 +1,31 @@
 package leetcode;
 
-fun threeSum(nums: IntArray): List<List<Int>> {
-    var list = mutableListOf<List<Int>>()
-    nums.sort()
-    for (var i = 0; i < nums.size - 2; i++) {
-        var l = i + 1
-        var r = nums.lastIndex
-        while (l < r) {
-            var sum = nums[i] + nums[l] + nums[r]
-            if (sum < 0)
-                l++
-            else if (sum > 0)
-                r--
-            else {
-                var e = listOf(nums[i], nums[l], nums[r])
-                if (!list.contains(e))
-                    list.add(e)
-                l++
-                r--
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class ThreeSum {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        var list = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+        for (var i = 0; i < nums.length - 2; i++) {
+            var l = i + 1;
+            var r = nums.length - 1;
+            while (l < r) {
+                var sum = nums[i] + nums[l] + nums[r];
+                if (sum < 0)
+                    l++;
+                else if (sum > 0)
+                    r--;
+                else {
+                    var e = List.of(nums[i], nums[l], nums[r]);
+                    if (!list.contains(e))
+                        list.add(e);
+                    l++;
+                    r--;
+                }
             }
         }
+        return list;
     }
-    return list
 }
--- leetcode/ThreeSumClosest.java
+++ leetcode/ThreeSumClosest.java
@@ -1,24 +1,26 @@
 package leetcode;
 
-import kotlin.math.abs
+import java.util.Arrays;
 
-fun threeSumClosest(nums: IntArray, target: Int): Int {
-    var delta = nums[0] + nums[1] + nums[2] - target
-    nums.sort()
-    for (var i = 0; i < nums.size - 2; i++) {
-        var l = i + 1
-        var r = nums.lastIndex
-        while (l < r) {
-            var sum = nums[i] + nums[l] + nums[r]
-            if (sum <= target - abs(delta))
-                l++
-            else if (sum >= target + abs(delta))
-                r--
-            else {
-                delta = sum - target
-                if (delta == 0) return target
+class ThreeSumClosest {
+    public static int threeSumClosest(int[] nums, int target) {
+        var delta = nums[0] + nums[1] + nums[2] - target;
+        Arrays.sort(nums);
+        for (var i = 0; i < nums.length - 2; i++) {
+            var l = i + 1;
+            var r = nums.length - 1;
+            while (l < r) {
+                var sum = nums[i] + nums[l] + nums[r];
+                if (sum <= target - Math.abs(delta))
+                    l++;
+                else if (sum >= target + Math.abs(delta))
+                    r--;
+                else {
+                    delta = sum - target;
+                    if (delta == 0) return target;
+                }
             }
         }
+        return target + delta;
     }
-    return target + delta
 }
