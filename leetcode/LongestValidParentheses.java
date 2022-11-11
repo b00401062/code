ea07d3fa2fe9fac483cde8a50b6f681e3c0bca2b
Wen-Bin Luo <b00401062@ntu.edu.tw>
2022-11-11 09:25:58 +0000

    Convert kt to java

--- leetcode/LongestValidParentheses.java
+++ leetcode/LongestValidParentheses.java
@@ -1,32 +1,34 @@
 package leetcode;
 
-import kotlin.math.max
-
-fun longestValidParentheses(s: String): Int {
-    var sb = StringBuilder(s)
-    do {
-        var changed = false
-        var lt_idx = -1
-        for ((i, c) in sb.withIndex()) {
-            if (c == '(') {
-                lt_idx = i
-            } else if (c == ')' && lt_idx != -1) {
-                sb[lt_idx] = '*'
-                sb[i] = '*'
-                lt_idx = -1
-                changed = true
+class LongestValidParentheses {
+    public static int longestValidParentheses(String s) {
+        var sb = new StringBuilder(s);
+        var changed = false;
+        do {
+            changed = false;
+            var lt_idx = -1;
+            for (var i = 0; i < sb.length(); i++) {
+                var c = sb.charAt(i);
+                if (c == '(') {
+                    lt_idx = i;
+                } else if (c == ')' && lt_idx != -1) {
+                    sb.setCharAt(lt_idx, '*');
+                    sb.setCharAt(i, '*');
+                    lt_idx = -1;
+                    changed = true;
+                }
             }
+        } while (changed);
+        var res = 0;
+        var cnt = 0;
+        for (var c : sb.toString().toCharArray()) {
+            if (c != '*') {
+                res = Math.max(res, cnt);
+                cnt = 0;
+            } else
+                cnt++;
         }
-    } while (changed)
-    var res = 0
-    var cnt = 0
-    for (c in sb) {
-        if (c != '*') {
-            res = max(res, cnt)
-            cnt = 0
-        } else
-            cnt++
+        res = Math.max(res, cnt);
+        return res;
     }
-    res = max(res, cnt)
-    return res
 }
