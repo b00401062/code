32b694a717d2435a0b3ed0e543b3c4b29eeda1be
Wen-Bin Luo <b00401062@ntu.edu.tw>
2022-11-11 12:57:40 +0000

    Convert kt to java

--- leetcode/Rotate.java
+++ leetcode/Rotate.java
@@ -1,19 +1,19 @@
 package leetcode;
 
-import kotlin.math.min
-
-fun rotate(matrix: Array<IntArray>): Unit {
-    var n = matrix.size
-    for (var i = 0; i < n - 1; i++) {
-        for (j in 0..min(i, n - i - 2)) {
-            var lt = matrix[i][j]
-            var ur = matrix[j][n - i - 1]
-            var rt = matrix[n - i - 1][n - j - 1]
-            var lr = matrix[n - j - 1][i]
-            matrix[i][j] = lr
-            matrix[j][n - i - 1] = lt
-            matrix[n - i - 1][n - j - 1] = ur
-            matrix[n - j - 1][i] = rt
+class Rotate {
+    public static void rotate(int[][] matrix) {
+        var n = matrix.length;
+        for (var i = 0; i < n - 1; i++) {
+            for (var j = 0; j <= Math.min(i, n - i - 2); j++) {
+                var lt = matrix[i][j];
+                var ur = matrix[j][n - i - 1];
+                var rt = matrix[n - i - 1][n - j - 1];
+                var lr = matrix[n - j - 1][i];
+                matrix[i][j] = lr;
+                matrix[j][n - i - 1] = lt;
+                matrix[n - i - 1][n - j - 1] = ur;
+                matrix[n - j - 1][i] = rt;
+            }
         }
     }
 }
--- leetcode/RotateRight.java
+++ leetcode/RotateRight.java
@@ -1,23 +1,25 @@
 package leetcode;
 
-fun rotateRight(head: ListNode?, k: Int): ListNode? {
-    if (head == null || head.next == null) return head
-    var n = 0
-    var ptr = head
-    var tail: ListNode? = null
-    while (ptr != null) {
-        n += 1
-        if (ptr.next == null) tail = ptr
-        ptr = ptr.next
+class RotateRight {
+    public static ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null) return head;
+        var n = 0;
+        var ptr = head;
+        var tail = (ListNode) null;
+        while (ptr != null) {
+            n += 1;
+            if (ptr.next == null) tail = ptr;
+            ptr = ptr.next;
+        }
+        if (k % n == 0) return head;
+        ptr = head;
+        for (var i = 0; i < n - k % n - 1; i++) {
+            ptr = ptr.next;
+        }
+        var newTail = ptr;
+        var newHead = ptr.next;
+        newTail.next = null;
+        tail.next = head;
+        return newHead;
     }
-    if (k % n == 0) return head
-    ptr = head
-    repeat(n - k % n - 1) {
-        ptr = ptr?.next
-    }
-    var newTail = ptr
-    var newHead = ptr?.next
-    newTail?.next = null
-    tail?.next = head
-    return newHead
 }
