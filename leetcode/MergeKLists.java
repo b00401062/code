d77b9a82c97662274fdafe7eafdea8c4c427f5c8
Wen-Bin Luo <b00401062@ntu.edu.tw>
2022-11-11 09:25:47 +0000

    Convert kt to java

--- leetcode/MergeKLists.java
+++ leetcode/MergeKLists.java
@@ -1,26 +1,29 @@
 package leetcode;
 
-private fun select(lists: Array<ListNode?>): Int {
-    var argmin: Int = -1
-    for (i in lists.indices)
-        argmin = when {
-            lists[i] == null -> argmin
-            argmin == -1 -> i
-            lists[argmin]!!.`val` < lists[i]!!.`val` -> argmin
-            else -> i
+class MergeKLists {
+    private static int select(ListNode[] lists) {
+        var argmin = -1;
+        for (var i = 0; i < lists.length; i++) {
+            argmin = (
+                lists[i] == null ? argmin :
+                argmin == -1 ? i :
+                lists[argmin].val < lists[i].val ? argmin :
+                i
+            );
         }
-    return argmin
-}
-
-fun mergeKLists(lists: Array<ListNode?>): ListNode? {
-    var head = ListNode(0)
-    var tail = head
-    var i: Int = select(lists)
-    while (i != -1) {
-        tail.next = ListNode(lists[i]!!.`val`)
-        tail = tail.next!!
-        lists[i] = lists[i]?.next
-        i = select(lists)
+        return argmin;
     }
-    return head.next
+
+    public static ListNode mergeKLists(ListNode[] lists) {
+        var head = new ListNode(0);
+        var tail = head;
+        var i = select(lists);
+        while (i != -1) {
+            tail.next = new ListNode(lists[i].val);
+            tail = tail.next;
+            lists[i] = lists[i].next;
+            i = select(lists);
+        }
+        return head.next;
+    }
 }
--- leetcode/MergeTwoLists.java
+++ leetcode/MergeTwoLists.java
@@ -1,24 +1,25 @@
 package leetcode;
 
-fun mergeTwoLists(l1: ListNode?, l2: ListNode?): ListNode? {
-    if (l1 == null) return l2
-    if (l2 == null) return l1
-    var head = ListNode(0)
-    var tail = head
-    var p1 = l1
-    var p2 = l2
-    while (p1 != null && p2 != null) {
-        if (p1.`val` < p2.`val`) {
-            tail.next = ListNode(p1.`val`)
-            tail = tail.next!!
-            p1 = p1.next
-        } else {
-            tail.next = ListNode(p2.`val`)
-            tail = tail.next!!
-            p2 = p2.next
+class MergeTwoLists {
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+        var head = new ListNode(0);
+        var tail = head;
+        var p1 = list1;
+        var p2 = list2;
+        while (p1 != null && p2 != null) {
+            if (p1.val < p2.val) {
+                tail.next = new ListNode(p1.val);
+                tail = tail.next;
+                p1 = p1.next;
+            } else {
+                tail.next = new ListNode(p2.val);
+                tail = tail.next;
+                p2 = p2.next;
+            }
         }
+        tail.next = (p1 == null) ? p2 : p1;
+        return head.next;
     }
-    if (p1 == null) tail.next = p2
-    else if (p2 == null) tail.next = p1
-    return head.next
 }
