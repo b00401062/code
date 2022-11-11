7d58b46b7322e90599b534cb6c515f9a11edcd7c
Wen-Bin Luo <b00401062@ntu.edu.tw>
2022-11-11 13:07:25 +0000

    Convert kt to java

--- leetcode/SolveSudoku.java
+++ leetcode/SolveSudoku.java
@@ -1,49 +1,55 @@
 package leetcode;
 
-private fun solve(
-    board: Array<CharArray>,
-    col: Array<BooleanArray>,
-    row: Array<BooleanArray>,
-    box: Array<BooleanArray>,
-    n: Int
-): Boolean {
-    var i = n / 9
-    var j = n % 9
-    if (n == 81)
-        return true
-    if (board[i][j] != '.')
-        return solve(board, col, row, box, n + 1)
-    var boxIndex = i / 3 * 3 + j / 3
-    var candidates = (0..8).filter { !(col[j][it] || row[i][it] || box[boxIndex][it]) }
-    for (candidate in candidates) {
-        board[i][j] = '1' + candidate
-        col[j][candidate] = true
-        row[i][candidate] = true
-        box[boxIndex][candidate] = true
-        if (solve(board, col, row, box, n + 1))
-            return true
-        board[i][j] = '.'
-        col[j][candidate] = false
-        row[i][candidate] = false
-        box[boxIndex][candidate] = false
-    }
-    return false
-}
+import java.util.stream.IntStream;
 
-fun solveSudoku(board: Array<CharArray>): Unit {
-    var col = Array(9) { BooleanArray(9) }
-    var row = Array(9) { BooleanArray(9) }
-    var box = Array(9) { BooleanArray(9) }
-    for (var i = 0; i < 9; i++) {
-        for (var j = 0; j < 9; j++) {
-            if (board[i][j] != '.') {
-                var num = board[i][j] - '1'
-                var boxIndex = i / 3 * 3 + j / 3
-                col[j][num] = true
-                row[i][num] = true
-                box[boxIndex][num] = true
+class SolveSudoku {
+    private static boolean solve(
+        char[][] board,
+        boolean[][] col,
+        boolean[][] row,
+        boolean[][] box,
+        int n
+    ) {
+        var i = n / 9;
+        var j = n % 9;
+        if (n == 81)
+            return true;
+        if (board[i][j] != '.')
+            return solve(board, col, row, box, n + 1);
+        var boxIndex = i / 3 * 3 + j / 3;
+        var candidates = IntStream.range(0, 9)
+            .filter((it) -> !(col[j][it] || row[i][it] || box[boxIndex][it]))
+            .toArray();
+        for (var candidate : candidates) {
+            board[i][j] = (char) ('1' + candidate);
+            col[j][candidate] = true;
+            row[i][candidate] = true;
+            box[boxIndex][candidate] = true;
+            if (solve(board, col, row, box, n + 1))
+                return true;
+            board[i][j] = '.';
+            col[j][candidate] = false;
+            row[i][candidate] = false;
+            box[boxIndex][candidate] = false;
+        }
+        return false;
+    }
+
+    public static void solveSudoku(char[][] board) {
+        var col = new boolean[9][9];
+        var row = new boolean[9][9];
+        var box = new boolean[9][9];
+        for (var i = 0; i < 9; i++) {
+            for (var j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    var num = board[i][j] - '1';
+                    var boxIndex = i / 3 * 3 + j / 3;
+                    col[j][num] = true;
+                    row[i][num] = true;
+                    box[boxIndex][num] = true;
+                }
             }
         }
+        solve(board, col, row, box, 0);
     }
-    solve(board, col, row, box, 0)
 }
