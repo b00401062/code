f84ecf33d5096e8f153389875fd405650ae107bb
Wen-Bin Luo <b00401062@ntu.edu.tw>
2022-11-11 03:21:51 +0000

    Convert kt to java

--- leetcode/Search.java
+++ leetcode/Search.java
@@ -1,18 +1,24 @@
 package leetcode;
 
-fun search(nums: IntArray, target: Int): Int {
-    val p = nums.size / 2
-    if (nums.size < 32)
-        return nums.indexOf(target)
-    else if (target == nums[0])
-        return 0
-    else if (target == nums[p])
-        return p
-    if ((target < nums[0]) xor (target < nums[p]) xor (nums[0] > nums[p])) {
-        val i = search(nums.sliceArray(1 until p), target)
-        return if (i == -1) -1 else 1 + i
-    } else {
-        val i = search(nums.sliceArray(p + 1 until nums.size), target)
-        return if (i == -1) -1 else p + 1 + i
+class Search {
+    private static int bruteForceSearch(int[] nums, int lt, int rt, int target) {
+        for (int i = lt; i < rt; i++) {
+            if (nums[i] == target) return i;
+        }
+        return -1;
+    }
+
+    private static int recursiveSearch(int[] nums, int lt, int rt, int target) {
+        int m = (lt + rt) / 2;
+        return (
+            rt - lt < 2 ? bruteForceSearch(nums, lt, rt, target) :
+            nums[m] == target ? m :
+            (nums[lt] <= target) ^ (target < nums[m]) ^ (nums[lt] <= nums[m]) ? recursiveSearch(nums, lt, m, target) :
+            recursiveSearch(nums, m + 1, rt, target)
+        );
+    }
+
+    public static int search(int[] nums, int target) {
+        return recursiveSearch(nums, 0, nums.length, target);
     }
 }
